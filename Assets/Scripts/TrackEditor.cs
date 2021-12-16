d04a2f8ee588d031f4eacbd06bd52dde29a53410
mys <m.grosser@seznam.cz>
2021-12-16 01:11:02 +0100

    Simplified thumbnail code

--- Assets/Scripts/TrackEditor.cs
+++ Assets/Scripts/TrackEditor.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
 
+// Taking a screenshot of a camera's Render Texture: https://docs.unity3d.com/ScriptReference/Camera.Render.html
+
 public class TrackEditor : MonoBehaviour
 {
-    // private List<GameObject> _parts = new();
-    // private List<Transform> _parts = new();
-    private Transform _parts;
+    [SerializeField]
+    private GameObject parts;
 
     void Start()
     {
-        _parts = GameObject.Find("Parts").transform.Find("Category0").transform;
+        // parts = GameObject.Find("Parts").transform.Find("Category0").transform;  // Now it's a prefab
         GenerateThumbnails();
     }
 
@@ -23,6 +24,11 @@ public class TrackEditor : MonoBehaviour
     {
         var thumbSize = 256;  // TODO: Should be relative to screen size
         var thumbSpacing = 10;  // Total space between two thumbnails
+        var rectSize = new Vector2(thumbSize, thumbSize);
+
+        var partsInstance = Instantiate(parts);
+        
+        var partsCategory0 = partsInstance.transform.Find("Category0").transform;
         
         // Create a render texture for the camera
         var renderTexture = new RenderTexture(thumbSize, thumbSize, 16)
@@ -36,61 +42,40 @@ public class TrackEditor : MonoBehaviour
         var cameraThumbCamera = cameraThumb.GetComponent<Camera>();
         cameraThumbCamera.targetTexture = renderTexture;
 
-        // Hide all parts, so just rendered object is visible at the moment
-        foreach (Transform part in _parts)
-        {
-            part.gameObject.SetActive(false);
-        }
-
         var i = 0;
-        foreach (Transform part in _parts)
+        foreach (Transform part in partsCategory0)
         {
             // Set camera position & look at the part
             cameraThumb.transform.position = part.position + new Vector3(-10, 10, -20);
             cameraThumb.transform.LookAt(part.position);
 
-            part.gameObject.SetActive(true);
-            var texture = RenderTextureImage(cameraThumbCamera);
-            part.gameObject.SetActive(false);
+            part.gameObject.SetActive(true);  // Show the part for render shot
+            RenderTexture.active = cameraThumbCamera.targetTexture;
+            cameraThumbCamera.Render();
+
+            // Create a new texture and read the active Render Texture into it.
+            var texture = new Texture2D(thumbSize, thumbSize);
+            texture.ReadPixels(new Rect(0, 0, thumbSize, thumbSize), 0, 0);
+            texture.Apply();
+
+            part.gameObject.SetActive(false);  // Hide the part after shot is taken
 
             // Create a UI thumbnail image for each part
             var imageThumb = new GameObject("imageThumb");
             imageThumb.transform.SetParent(GameObject.Find("Canvas").transform);
             imageThumb.AddComponent<Image>();
             var imageThumbImage = imageThumb.GetComponent<Image>();
-            var sprite = Sprite.Create(texture,
-                new Rect(0, 0, thumbSize, thumbSize), new Vector2());
+            var sprite = Sprite.Create(texture, new Rect(0, 0, thumbSize, thumbSize), Vector2.zero);
             imageThumbImage.sprite = sprite;
-
-            ++i;
 
             // imageThumbImage.transform.position = new Vector3(i * thumbSize + 10, thumbSize + 10, 0);  // also works
             var rectTransform = imageThumb.GetComponent<RectTransform>();
-            rectTransform.transform.position = new Vector3(i * (thumbSize + thumbSpacing) - thumbSize * .5f, thumbSize * .5f + thumbSpacing, 0);
-            rectTransform.sizeDelta = new Vector2(thumbSize, thumbSize);
+            rectTransform.transform.position = new Vector3(i * (thumbSize + thumbSpacing) + thumbSize * .5f, thumbSize * .5f + thumbSpacing, 0);
+            rectTransform.sizeDelta = rectSize;
+
+            ++i;
         }
 
-        GameObject.Find("Parts").SetActive(false);
-
-        /*  Take a "screenshot" of a camera's Render Texture.  */
-        Texture2D RenderTextureImage(Camera cam)  //  https://docs.unity3d.com/ScriptReference/Camera.Render.html
-        {
-            // The Render Texture in RenderTexture.active is the one that will be read by ReadPixels.
-            // var currentRenderTexture = RenderTexture.active;
-
-            RenderTexture.active = cam.targetTexture;
-
-            // Render the camera's view.
-            cam.Render();
-
-            // Make a new texture and read the active Render Texture into it.
-            Texture2D texture = new Texture2D(thumbSize, thumbSize);
-            texture.ReadPixels(new Rect(0, 0, thumbSize, thumbSize), 0, 0);
-            texture.Apply();
-
-            // Replace the original active Render Texture.
-            // RenderTexture.active = currentRenderTexture;
-            return texture;
-        }
+        partsInstance.SetActive(false);
     }
 }
