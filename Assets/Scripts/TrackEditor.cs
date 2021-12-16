335e8a84f2d7fb4bbb0872ab5d07257b1d66d9d3
mys <m.grosser@seznam.cz>
2021-12-16 02:20:06 +0100

    Adds thumbnail events & new part instantiation

--- Assets/Scripts/TrackEditor.cs
+++ Assets/Scripts/TrackEditor.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 // Taking a screenshot of a camera's Render Texture: https://docs.unity3d.com/ScriptReference/Camera.Render.html
 
+// TODO: Consider using Button (legacy) - Does it cause less draw calls than Button with Text mesh pro? (which is probably, what's used)
+
 public class TrackEditor : MonoBehaviour
 {
     [SerializeField]
     private GameObject parts;
+    private Transform _partsCategory0;  // Transform is iterable. Use GetChild(index) to get n-th child.
 
     void Start()
     {
@@ -28,7 +32,7 @@ public class TrackEditor : MonoBehaviour
 
         var partsInstance = Instantiate(parts);
         
-        var partsCategory0 = partsInstance.transform.Find("Category0").transform;
+        _partsCategory0 = partsInstance.transform.Find("Category0").transform;
         
         // Create a render texture for the camera
         var renderTexture = new RenderTexture(thumbSize, thumbSize, 16)
@@ -37,13 +41,12 @@ public class TrackEditor : MonoBehaviour
         };
 
         // Create a camera for shooting parts
-        var cameraThumb = new GameObject("cameraThumbnail");
-        cameraThumb.AddComponent<Camera>();
+        var cameraThumb = new GameObject("cameraThumbnail", typeof(Camera));
         var cameraThumbCamera = cameraThumb.GetComponent<Camera>();
         cameraThumbCamera.targetTexture = renderTexture;
 
         var i = 0;
-        foreach (Transform part in partsCategory0)
+        foreach (Transform part in _partsCategory0)
         {
             // Set camera position & look at the part
             cameraThumb.transform.position = part.position + new Vector3(-10, 10, -20);
@@ -60,22 +63,32 @@ public class TrackEditor : MonoBehaviour
 
             part.gameObject.SetActive(false);  // Hide the part after shot is taken
 
-            // Create a UI thumbnail image for each part
-            var imageThumb = new GameObject("imageThumb");
-            imageThumb.transform.SetParent(GameObject.Find("Canvas").transform);
-            imageThumb.AddComponent<Image>();
-            var imageThumbImage = imageThumb.GetComponent<Image>();
+            // Create a UI thumbnail (button with image) for each part
             var sprite = Sprite.Create(texture, new Rect(0, 0, thumbSize, thumbSize), Vector2.zero);
-            imageThumbImage.sprite = sprite;
+            var buttonThumb = new GameObject($"buttonThumb_{i}", typeof(Button), typeof(Image));
+            buttonThumb.transform.SetParent(GameObject.Find("Canvas").transform);
+            buttonThumb.GetComponent<Image>().sprite = sprite;
 
             // imageThumbImage.transform.position = new Vector3(i * thumbSize + 10, thumbSize + 10, 0);  // also works
-            var rectTransform = imageThumb.GetComponent<RectTransform>();
+            var rectTransform = buttonThumb.GetComponent<RectTransform>();
             rectTransform.transform.position = new Vector3(i * (thumbSize + thumbSpacing) + thumbSize * .5f, thumbSize * .5f + thumbSpacing, 0);
             rectTransform.sizeDelta = rectSize;
+            
+            buttonThumb.GetComponent<Button>().onClick.AddListener(PartSelect);
+
+            // imageThumb.AddComponent<EventTrigger>();
+            // var eventTrigger = imageThumb.GetComponent<EventTrigger>();
 
             ++i;
         }
 
         partsInstance.SetActive(false);
+    }
+
+    void PartSelect()
+    {
+        var buttonNameParsed = EventSystem.current.currentSelectedGameObject.name.Split('_');
+        int partNo = int.Parse(buttonNameParsed[1]);
+        var newPart = Instantiate(_partsCategory0.GetChild(partNo));
     }
 }
