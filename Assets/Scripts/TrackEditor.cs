64ceff832bdda99be5a02136df1a1a31b819238b
mys <m.grosser@seznam.cz>
2021-12-17 03:09:36 +0100

    Implements selection control

--- Assets/Scripts/TrackEditor.cs
+++ Assets/Scripts/TrackEditor.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 // Taking a screenshot of a camera's Render Texture: https://docs.unity3d.com/ScriptReference/Camera.Render.html
@@ -9,10 +10,14 @@ using UnityEngine.UI;
 
 public class TrackEditor : MonoBehaviour
 {
-    [SerializeField]  // prefab
-    private GameObject parts;
-    [SerializeField]  // prefab
-    private GameObject gridCube;
+    [SerializeField]
+    private GameObject partsPrefab;
+    [SerializeField]
+    private GameObject gridCubePrefab;
+    [SerializeField]
+    private GameObject selectionCubePrefab;
+    private GameObject _selectionCube;
+    private V3 _selectionCubeCoords;
     private Transform _partsCategory0;  // Transform is iterable. Use GetChild(index) to get n-th child.
     private List<List<List<Vector3>>> _grid = new();  // 3D grid of coordinates
     private V3 _origin;  // coordinates of the origin in _grid, i.e. lists indexes of the center cube
@@ -20,10 +25,21 @@ public class TrackEditor : MonoBehaviour
 
     void Start()
     {
+        var ui = GameObject.Find("Canvas");
+        ui.transform.Find("buttonUp").GetComponent<Button>().onClick.AddListener(MoveCameraTarget);
+        ui.transform.Find("buttonDown").GetComponent<Button>().onClick.AddListener(MoveCameraTarget);
+        ui.transform.Find("buttonLeft").GetComponent<Button>().onClick.AddListener(MoveCameraTarget);
+        ui.transform.Find("buttonRight").GetComponent<Button>().onClick.AddListener(MoveCameraTarget);
+        ui.transform.Find("buttonCloser").GetComponent<Button>().onClick.AddListener(MoveCameraTarget);
+        ui.transform.Find("buttonFarther").GetComponent<Button>().onClick.AddListener(MoveCameraTarget);
+        
+        _selectionCube = Instantiate(selectionCubePrefab);
         _camera = GameObject.Find("CameraEditor");
         _camera.transform.LookAt(Vector3.zero);
         GenerateThumbnails();
         GenerateGrid();
+        _selectionCube.SetActive(true);
+        SetSelectionCoords(V3.zero);
     }
 
     // void FixedUpdate()
@@ -35,7 +51,7 @@ public class TrackEditor : MonoBehaviour
         const int thumbSpacing = 5;  // Total space between two thumbnails
         var rectSize = new Vector2(thumbSize, thumbSize);  // How can I make it const?
 
-        var partsInstance = Instantiate(parts);
+        var partsInstance = Instantiate(partsPrefab);
         
         _partsCategory0 = partsInstance.transform.Find("Category0").transform;
         
@@ -45,7 +61,7 @@ public class TrackEditor : MonoBehaviour
             antiAliasing = 2,
         };
 
-        // Create a camera for shooting parts
+        // Create a camera for shooting partsPrefab
         var cameraThumb = new GameObject("cameraThumbnail", typeof(Camera));
         var cameraThumbCamera = cameraThumb.GetComponent<Camera>();
         cameraThumbCamera.targetTexture = renderTexture;
@@ -93,30 +109,27 @@ public class TrackEditor : MonoBehaviour
     void GenerateGrid()
     {
         const int cubeSize = 10;
-        const int xCount = 7;
-        const int yCount = 5;
-        const int zCount = 5;
 
-        _origin = new V3(xCount / 2, yCount / 2, zCount / 2);  // Assumes the counts are all even
+        _origin = new V3(V3.xCount / 2, V3.yCount / 2, V3.zCount / 2);  // Assumes the counts are all even
 
         var gridParent = new GameObject("gridParent");
-        int i = 0;
-        for (int z = 0; z < zCount; ++z)
+
+        for (int z = 0; z < V3.zCount; ++z)
         {
             var yCubes = new List<List<Vector3>>();
-            for (int y = 0; y < yCount; ++y)
+            for (int y = 0; y < V3.yCount; ++y)
             {
                 var xCubes = new List<Vector3>();
-                for (int x = 0; x < xCount; ++x)
+                for (int x = 0; x < V3.xCount; ++x)
                 {
                     var coordinates = new Vector3(
-                        x * cubeSize - xCount * cubeSize / 2,
-                        y * cubeSize - yCount * cubeSize / 2,
-                        z * cubeSize - zCount * cubeSize / 2);
+                        x * cubeSize - V3.xCount * cubeSize / 2,
+                        y * cubeSize - V3.yCount * cubeSize / 2,
+                        z * cubeSize - V3.zCount * cubeSize / 2);
 
                     xCubes.Add(coordinates);
 
-                    var cube = Instantiate(gridCube, gridParent.transform);
+                    var cube = Instantiate(gridCubePrefab, gridParent.transform);
                     cube.transform.position = coordinates;
                     cube.transform.localScale = new Vector3(cubeSize, cubeSize, cubeSize);
                 }
@@ -126,9 +139,10 @@ public class TrackEditor : MonoBehaviour
         }
     }
 
-    void PositionToGrid(GameObject obj, V3 position)
+    Vector3 PositionToGrid(GameObject obj, V3 position)
     {
         obj.transform.position = _grid[position.x][position.y][position.z];
+        return obj.transform.position;
     }
 
     void AddPart()
@@ -140,5 +154,41 @@ public class TrackEditor : MonoBehaviour
 
         // PositionToGrid(newPart, _origin);
         PositionToGrid(newPart, new V3());
+    }
+
+    void MoveCameraTarget()
+    {
+        var buttonName = EventSystem.current.currentSelectedGameObject.name;
+
+        if (buttonName == "buttonUp")
+        {
+            SetSelectionCoords(_selectionCubeCoords.MoveUp());
+        }
+        else if (buttonName == "buttonDown")
+        {
+            SetSelectionCoords(_selectionCubeCoords.MoveDown());
+        }
+        else if (buttonName == "buttonLeft")
+        {
+            SetSelectionCoords(_selectionCubeCoords.MoveLeft());
+        }
+        else if (buttonName == "buttonRight")
+        {
+            SetSelectionCoords(_selectionCubeCoords.MoveRight());
+        }
+        else if (buttonName == "buttonCloser")
+        {
+            SetSelectionCoords(_selectionCubeCoords.MoveCloser());
+        }
+        else if (buttonName == "buttonFarther")
+        {
+            SetSelectionCoords(_selectionCubeCoords.MoveFarther());
+        }
+    }
+
+    void SetSelectionCoords(V3 coords)
+    {
+        _selectionCubeCoords = coords;
+        _camera.transform.LookAt(PositionToGrid(_selectionCube, coords));
     }
 }
--- Assets/Scripts/V3.cs
+++ Assets/Scripts/V3.cs
@@ -4,11 +4,20 @@
 ///     Floats are rounded down.
 /// </para>
 /// </summary>
+
+// TODO: x & z coords are swapped or what (see MoveLeft() v MoveCloser)
+
 internal struct V3
 {
     public int x;
     public int y;
     public int z;
+
+    public static V3 zero = new(0, 0, 0);
+    
+    public static int xCount = 7;
+    public static int yCount = 5;
+    public static int zCount = 5;
 
     public V3(int x, int y, int z)  // This automatically rounds a float down.
     {
@@ -16,4 +25,53 @@ internal struct V3
         this.y = y;
         this.z = z;
     }
+
+    public V3 MoveUp()
+    {
+        ++y;
+        if (y == yCount)
+            y = 0;
+        return this;
+    }
+
+    public V3 MoveDown()
+    {
+        --y;
+        if (y == -1)
+            y = yCount - 1;
+        return this;
+    }
+
+    public V3 MoveLeft()
+    {
+        --z;
+        if (z == -1)
+            z = xCount - 1;
+        return this;
+    }
+
+    public V3 MoveRight()
+    {
+        ++z;
+        if (z == xCount)
+            z = 0;
+        return this;
+    }
+
+    public V3 MoveCloser()
+    {
+        --x;
+        if (x == -1)
+            x = zCount - 1;
+        return this;
+    }
+
+    public V3 MoveFarther()
+    {
+        ++x;
+        if (x == zCount)
+            x = 0;
+        return this;
+    }
+
 }
